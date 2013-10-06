c640568deba24cbfacc4619c88cd8e020ade6584
Keith Pitty <keith@keithpitty.com>
2013-10-06 19:00:40 +1100

    Validate uniqueness of player number.

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,9 +2,8 @@ class Player < ActiveRecord::Base
   has_many :player_rounds
   has_many :rounds, :through => :player_rounds
   has_many :scores
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :player_number, presence: true
+  validates :first_name, :last_name, :player_number, presence: true
+  validates :player_number, uniqueness: true
 
   def name
     "#{first_name} #{last_name}"
