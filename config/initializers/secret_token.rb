c5318209efa1b56d08802e71d47fe61273dc2756
Keith Pitty <keith@keithpitty.com>
2013-10-06 11:43:44 +1100

    Initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skinners2::Application.config.secret_key_base = 'a9d41fa3aca1ed2782cfc28857816731b6d8a3d6ed5f25c68d118282f94821e21b9c0072c8abc34f06ddce08036fa7e70a69f1add72c2fdc8151cefd3a060b1f'
